223407d96d77acdec5d81458f8f359d7dc60200c
adi.farhan <adi.farhan@bandung.go.id>
2023-07-14 23:03:49 +0700

    add google analytic

--- index.php
+++ index.php
@@ -2,6 +2,16 @@
 <html lang="en" data-bs-theme="dark">
 
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-FW5PN4B7XE"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-FW5PN4B7XE');
+    </script>
+    
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Undangan Pernikahan Adi & Ajeng</title>
